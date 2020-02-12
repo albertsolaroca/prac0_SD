3263e6e16b7ca3d3de2046051b0e1bac0646b0b8
ALBERT SOLA ROCA <asolaroc7.alumnes@ub052229.mat.ub.edu>
2020-02-12 18:16:41 +0100

    Write test read test done

--- src/main/java/utils/ComUtilsService.java
+++ src/main/java/utils/ComUtilsService.java
@@ -12,12 +12,34 @@ public class ComUtilsService {
     }
 
     public void writeTest() {
-        //TODO: put your code here
+        String nom = "Albert Sola Roca";
+        int edat = 22;
+        String comment = "Ruben xq me abandonaste puto :v";
+        try {
+            comUtils.write_string_variable(2,nom);
+            comUtils.write_int32(edat);
+            comUtils.write_string(comment);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 
     public String readTest() {
         String result = "";
-        //TODO: put your code here
+        String nom = "";
+        int edat = 0;
+        String comment = "";
+        try {
+            nom = comUtils.read_string_variable(2);
+            edat = comUtils.read_int32();
+            comment= comUtils.read_string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        result += nom + " ";
+        result += edat + " ";
+        result += comment + " ";
         return result;
     }
 
